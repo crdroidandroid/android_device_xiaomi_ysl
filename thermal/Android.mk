511adcf36fd52557cecb4e7504fd5a8b0c9c73ff
RevanthTolety <crevanthtolety@gmail.com>
2019-09-02 20:15:55 +0530

    ysl: Update Thermal Hal Taken From LA.UM.7.6.2.r1-09500-89xx.0

--- thermal/Android.mk
+++ thermal/Android.mk
@@ -21,8 +21,10 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_SRC_FILES := thermal.c
-LOCAL_SRC_FILES += thermal-8953.c
+LOCAL_SRC_FILES += thermal_common.c
+LOCAL_SRC_FILES += thermal_target.c
 
+LOCAL_HEADER_LIBRARIES := libutils_headers libhardware_headers
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter
